554c672c7b9cb45d411769d9b71180f8d3f4632f
Web Addicto <webaddicto@users.noreply.github.com>
2017-02-06 22:12:29 +0100

    Updated the code

--- src/SecurityPlugin.php
+++ src/SecurityPlugin.php
@@ -48,3 +48,50 @@ class SecurityPlugin extends AbstractPlugin {
 		if(!filter_var($url, FILTER_VALIDATE_URL)){
 			$this->ErrorMsg("URL is not valid");
 		}
+		
+		// Do not proxify invalid scheme
+		// *** May not be needed because the php-proxy already adds http:// by default ***
+		if(!in_array(strtolower(parse_url($url, PHP_URL_SCHEME)), array("http","https","ftp"))){
+			$this->ErrorMsg("Scheme is not allowed");
+		}
+		
+		// Remove "www." from host
+		$url_host = preg_replace('/^www\./is', '', trim(parse_url($url, PHP_URL_HOST)));
+		
+		// Do not proxify localhost
+		if(preg_match('/^localhost/is', $url_host)){
+			$this->ErrorMsg("Host is not allowed");
+		}
+		
+		// Do not proxify internal IP addresses
+		if(filter_var($url_host, FILTER_VALIDATE_IP)){
+			if(filter_var($url_host, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) === false){
+				$this->ErrorMsg("Host is not allowed");
+			}
+		}
+		
+		// Do not proxify our own proxy host ($_SERVER['HTTP_HOST'])
+		if(stripos($url_host, $_SERVER['HTTP_HOST']) === 0){
+			$this->ErrorMsg("Host is not allowed");
+		}
+		
+		// Remove "www." from app_url()
+		$app_host = preg_replace('/^www\./is', '', trim(parse_url(app_url(), PHP_URL_HOST)));
+		
+		// Do not proxify our own proxy host (app_url())
+		if(stripos($url_host, $app_host) === 0){
+			$this->ErrorMsg("Host is not allowed");
+		}
+		
+		// Do not proxify the server's IP address
+		if(filter_var($_SERVER['SERVER_ADDR'], FILTER_VALIDATE_IP)){
+		    if($url_host == $_SERVER['SERVER_ADDR']){
+				$this->ErrorMsg("Host is not allowed");
+			}
+		} 
+		
+	}
+	
+}
+
+?>
