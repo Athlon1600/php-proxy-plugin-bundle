a89e1b2329368ad0bd559b1e913cc55cedf4a51f
Athlon1600 <Athlon2400@gmail.com>
2018-02-23 21:32:17 -0600

    fixed thumb proxification for youtube.com

--- src/YoutubePlugin.php
+++ src/YoutubePlugin.php
@@ -29,8 +29,23 @@ class YoutubePlugin extends AbstractPlugin {
 		// do this on all youtube pages
 		$output = preg_replace('@masthead-positioner">@', 'masthead-positioner" style="position:static;">', $output, 1);
 		
-		// replace future thumbnails with src=
-		$output = preg_replace('#<img[^>]*data-thumb=#s','<img alt="Thumbnail" src=', $output);
+		// data-thumb holds real image when it is available!
+		$output = preg_replace_callback('/<img[^>]+data-thumb="(.*?)"[^>]*/is', function($matches){
+			
+			// may or may not have src= attribute
+			$has_src = strpos($matches[0], 'src="') !== false;
+			
+			// proxified thumb url
+			$thumb_url = $matches[1]; //proxify_url($matches[1], false);
+			
+			if($has_src){
+				// TODO: maybe remove data-thumb too?
+				$matches[0] = str_replace('data-thumb', 'remove-this', $matches[0]);
+				return preg_replace('/src="(.*?)"/i', 'src_replaced="1" src="'.$thumb_url.'"', $matches[0]);
+			}
+			
+			return preg_replace('/data-thumb="(.*?)"/i', 'src_added="1" src="'.$thumb_url.'"', $matches[0]);
+		}, $output);
 		
 		$youtube = new \YouTubeDownloader();
 		// cannot pass HTML directly because all the links in it are already "proxified"...
