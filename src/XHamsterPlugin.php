0f00c5d1c811b991213c98e4e5d6d70822d14c55
volkirik <33526598+volkirik@users.noreply.github.com>
2018-06-19 22:05:23 +0300

    Add files via upload

--- src/XHamsterPlugin.php
+++ src/XHamsterPlugin.php
@@ -4,52 +4,69 @@ namespace Proxy\Plugin;
 
 use Proxy\Plugin\AbstractPlugin;
 use Proxy\Event\ProxyEvent;
+
 use Proxy\Html;
 
 class XHamsterPlugin extends AbstractPlugin {
 
 	protected $url_pattern = 'xhamster.com';
 	
-	public function onBeforeRequest(ProxyEvent $event){
-		// we do not want to force mobile this time
-		$event['request']->headers->set('user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0');
-	}
-	
 	private function find_video($html){
+
 		$file = false;
 		
-		if(preg_match('/mp4File":"([^"]+)/', $html, $matches)){
-			$file = $matches[1];
-			return stripslashes($file);
+		if(preg_match("/\"mp4File\"\s*:\s*\"([^\"]+)\"/", $html, $matches)){
+			$file = rawurldecode(trim($matches[1]));
+
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $file);
+			curl_setopt($ch, CURLOPT_HEADER, 1);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0');
+			curl_setopt($ch, CURLOPT_REFERER, $this->base_url);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, False);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, False);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, False);
+			$result = curl_exec($ch); 
+			curl_close($ch);
+			
+			$file = preg_match('/Location\:\s*(.+?)($|\n)/is', $result, $matches) ? $matches[1] : $file;
+			
+		} else if(preg_match("/\"480p\"\:\"(.+?)\"/s", $html, $matches)){
+			$file = rawurldecode(stripslashes(trim($matches[1])));
+		} else if(preg_match("/\"720p\"\:\"(.+?)\"/s", $html, $matches)){
+			$file = rawurldecode(stripslashes(trim($matches[1])));
 		}
 		
 		return $file;
 	}
 	
+	private function img_sprite($matches){
+		return str_replace($matches[1], proxify_url($matches[1], $matches[1]), $matches[0]);
+	}
+
 	public function onCompleted(ProxyEvent $event){
+	
 		$response = $event['response'];
 		$content = $response->getContent();
+		$this->base_url = $event['request']->getUri();
 		
-		// remove ads
-		$content = HTML::remove('.ts', $content);
+		// remove ts_popunder stuff
+		$content = preg_replace('/<script[^>]*no-popunder[^>]*><\/script>/m', '', $content);
 		
-		// is this video page?
+		$content = preg_replace_callback('/<img[^>]*sprite=\'(.*?)\'/im', array($this, 'img_sprite'), $content);
+		
+		// are we on a video page?
 		$file = $this->find_video($content);
-		if($file){
-			$player = vid_player($file, 950, 650);
-			$player = str_replace('<video', '<video style="display:block;', $player);
-			
-			//$content = HTML::replace_inner('#video_box', $player, $content);
-			$content = HTML::replace_inner('#player-container', $player, $content);
-			
-			// remove "show comments" button
-			$content = HTML::remove('#commentToggle', $content);
-			
-			// display all comments by default
-			$content = str_replace('<div class="comments_block"', '<div style="display:block;" class="comments_block"', $content);
-		}
 		
-		$content = Html::remove_scripts($content);
+		if($file){
+		
+			$player = vid_player($file, 638, 504);
+			
+			$content = Html::replace_inner("#playerSwf", $player, $content);
+		}
 		
 		$response->setContent($content);
 	}
